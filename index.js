07678f146a999956a46a99f805e2952c2af45dc4
Robert Egge <robertegge@leslies-air-2.mynetworksettings.com>
2023-11-20 12:13:39 -0500

    RJE first commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 'use strict'
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+// route
+app.get("/",(req, res)=>{
+  res.send("hello world")
+})
+
+// app listen
+
+
+app.listen(PORT);
