4502b2bd4e5a9d31cd41362c34da5acd1c5109f2
William Egge <lockegge@gmail.com>
2023-12-06 20:37:01 -0500

    add winston and cors

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 "use strict";
 
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
+const express = require("express"); // import express
+const mongoose = require("mongoose"); // import mongoose
+const cors = require("cors"); // import cors
+const winston = require("winston"); // import winston
+const app = express(); // create express app
 require("dotenv").config();
 
+
+const corsOptions = {
+  origin: 'https://task-app-page.onrender.com/', // or the URL of your frontend application
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+
+// Middleware
 app.use(express.json());
+app.use(cors(corsOptions));
+
 
 // MongoDB Atlas Database Connection String
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.llplsha.mongodb.net/?retryWrites=true&w=majority`;
 
+// Connect to MongoDB Atlas
 async function connectDB() {
   try {
     await mongoose.connect(uri);
-    console.log("MongoDB Connected");
+    logger.log("info", "MongoDB Connected");
   } catch (err) {
-    console.log(`Mongoose connection error: ${err}`);
+    logger.error(`Mongoose connection error: ${err}`);
     process.exit(1);
   }
 }
 
+// Call the connectDB function
 connectDB();
 
 // Define a schema
@@ -35,15 +49,41 @@ const TaskSchema = new mongoose.Schema({
   },
 });
 
+// Compile our model
 const Task = mongoose.model("Task", TaskSchema);
 
-mongoose.connection.on("error", (err) => {
-  console.log(`Mongoose connection error: ${err}`);
+// winston logger
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  defaultMeta: { service: "task-manager" },
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
 });
 
+// If not in production, then log to the `console`
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+
+// Mongoose connection events
+mongoose.connection.on("error", (err) => {
+  logger.error(`Mongoose connection error: ${err}`);
+});
+
+// When the connection is disconnected
 process.on("SIGINT", async () => {
   await mongoose.connection.close();
-  console.log(
+  logger.error(
     "Mongoose default connection disconnected through app termination"
   );
   process.exit(0);
@@ -51,20 +91,29 @@ process.on("SIGINT", async () => {
 
 // Routes
 
+/**
+ * A root route that returns "hello world"
+ */
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
+/**
+ * Gets all tasks that are not completed
+ */
 app.get("/api/tasks", async (req, res) => {
   try {
-    const tasks = await Task.find({ completed: "false" });
+    const tasks = await Task.find({ completed: false });
     res.json(tasks);
   } catch (err) {
-    console.log(err);
+    logger.error(err);
     res.status(500).send({ message: err.message });
   }
 });
 
+/**
+ * Posts a new task
+ */
 app.post("/api/tasks", async (req, res) => {
   try {
     const task = await Task.create({
@@ -74,11 +123,14 @@ app.post("/api/tasks", async (req, res) => {
     });
     res.json(task);
   } catch (err) {
-    console.log(err);
+    logger.error(err);
     res.status(500).send({ message: err.message });
   }
 });
 
+/**
+ * Updates a task by id
+ */
 app.put("/api/tasks/:id", async (req, res) => {
   const taskId = req.params.id;
   if (!mongoose.Types.ObjectId.isValid(taskId)) {
@@ -91,11 +143,14 @@ app.put("/api/tasks/:id", async (req, res) => {
     });
     res.json(task);
   } catch (err) {
-    console.log(err);
+    logger.error(err);
     res.status(500).send({ message: err.message });
   }
 });
 
+/**
+ * Deletes a task by id
+ */
 app.delete("/api/tasks/:id", async (req, res) => {
   const taskId = req.params.id;
 
@@ -112,11 +167,14 @@ app.delete("/api/tasks/:id", async (req, res) => {
 
     res.status(200).send({ message: "Task deleted successfully" });
   } catch (err) {
-    console.log(err);
+    logger.error(err);
     res.status(500).send({ message: err.message });
   }
 });
 
+/**
+ * Starts the local server
+ */
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`server is running on port ${process.env.PORT}`);
+  logger.log("info", `server is running on port ${process.env.PORT}`);
 });
